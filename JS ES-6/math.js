9dd20454ec1d87e992b2fc6e40c1bf7c58a51779
Akshay Neje <akshayneje2823@gmail.com>
2022-04-23 18:35:39 +0530

    ES-6

---
+++ JS ES-6/math.js
@@ -0,0 +1,7 @@
+// Marth ==>
+
+console.log(Math.max(11,32,33))
+console.log(Math.sqrt(16))        /* returns Square Root ==> 4 */
+console.log(Math.cbrt(64))        /* returns cube Root ==> 4 */
+console.log(Math.ceil(69.7))        /* returns Aapproximate number ==> 70 */
+console.log(Math.floor(69.9))        /* returns Approximate Number it wont take nearest no==> 4 */
---
+++ JS ES-6/spred_op.js
@@ -0,0 +1,12 @@
+//  Spread Oprator Can be Aplied Only Arrays[] And Object{}....
+
+// With Arrays
+var arr = [1,2,3,4,5,6,7]
+ var arr2 =[...arr,8,9,10]
+
+ console.log(arr2)
+
+var obj = {name:"AKshay",id:101,email:"name@gmail.com"}
+
+var obj2 = {...obj,city:"Kolhapur"}
+console.log(obj2)
