b299e78613549933e226a4f45f27b31638276b68
Akshay Neje <akshayneje2823@gmail.com>
2022-09-14 22:40:11 +0530

    Done

--- ES6 Functions Methods/call.js
+++ ES6 Functions Methods/call.js
@@ -1,27 +1,36 @@
+// // var person = {
+// //     name : "Akshay",
+// //     hello : function(thing){
+// //         console.log(this.name + " Hello "+ thing )
+// //     }
+// // }
+// // person.hello("Good Evening")
+
+
+
+
+// // Call Method
 // var person = {
 //     name : "Akshay",
 //     hello : function(thing){
-//         console.log(this.name + " Hello "+ thing )
+//         console.log(this.name + " Says Hello, "+ thing )
 //     }
 // }
-// person.hello("Good Evening")
 
+// var anotherObject = {
+//     name:"Sagar"
+// }
 
-
-
-// Call Method
-var person = {
-    name : "Akshay",
-    hello : function(thing){
-        console.log(this.name + " Says Hello, "+ thing )
-    }
-}
-
-var anotherObject = {
-    name:"Sagar"
-}
-
-person.hello.call(anotherObject,"Good Evening");
+// person.hello.call(anotherObject,"Good Evening");
 
 // Call methos invokes a function and let us allows 
 // to pass an arguments as one by one
+
+
+
+
+
+
+
+
+
--- Pattern Program/square.js
+++ Pattern Program/square.js
@@ -1,41 +1,70 @@
-// let n = 5; // row or column count
-// // defining an empty string
-// let string = "";
+// // let n = 5; // row or column count
+// // // defining an empty string
+// // let string = "";
 
-// for(let i = 0; i < n; i++) { // external loop
-//   for(let j = 0; j < n; j++) { // internal loop
-//     string += "*";
-//   }
-//   // newline after each row
-//   string += "\n";
+// // for(let i = 0; i < n; i++) { // external loop
+// //   for(let j = 0; j < n; j++) { // internal loop
+// //     string += "*";
+// //   }
+// //   // newline after each row
+// //   string += "\n";
+// // }
+// // // printing the string
+// // console.log(string);
+
+
+
+// // let n = 5; 
+// // let string = "";
+
+// // for(let i = 0; i < n; i++) { 
+// //   for(let j = 0; j < n; j++) { 
+// //     string += "*";
+// //   }
+// //   string += "\n";
+// // }
+// // console.log(string);
+
+
+
+
+
+
+// let b = 8;
+// let str = "";
+// for (let i = 0; i < b; i++) {
+//     for (let j = 0; j < b; j++) {
+//         str =str + "*";
+//     }
+//   str = str + "\n"
 // }
-// // printing the string
-// console.log(string);
+// console.log(str)
 
 
 
-let n = 5; 
-let string = "";
 
-for(let i = 0; i < n; i++) { 
-  for(let j = 0; j < n; j++) { 
-    string += "*";
-  }
-  string += "\n";
+
+
+// let str = "";
+
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     str =str + "*";
+//   }
+//   str = str + "\n"
+// }
+// console.log(str)
+
+
+
+let array = [1,2,3,4]
+
+
+for (let i = 0; i < array.length; i++) {
+  // let ans = array[i]*2+10
+  // console.log(ans)
+  console.log(i)
 }
-console.log(string);
 
 
 
-
-
-
-let b = 8;
-let str = "";
-for (let i = 0; i < b; i++) {
-    for (let j = 0; j < b; j++) {
-        str =str + "*";
-    }
-  str = str + "\n"
-}
-console.log(str)
